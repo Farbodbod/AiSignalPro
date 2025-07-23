1419d57c16acd45a2af1aa4723f34b86d7dc1a00
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-07-23 04:26:44 +0330

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,15 +1,10 @@
 from django.http import JsonResponse
 
 def system_status_view(request):
-    """
-    یک نمای ساده که وضعیت اتصال به سرویس‌ها را برمی‌گرداند.
-    """
     data = [
-      { "name": 'Kucoin', "status": 'online', "ping": '120ms' },
-      { "name": 'Gate.io', "status": 'online', "ping": '180ms' },
-      { "name": 'Telegram', "status": 'offline', "ping": '---' },
-      { "name": 'Coingecko', "status": 'online', "ping": 'OK' },
+        {"name": "Kucoin", "status": "online", "ping": "120ms"},
+        {"name": "Gate.io", "status": "online", "ping": "180ms"},
+        {"name": "Telegram", "status": "offline", "ping": "---"},
+        {"name": "Coingecko", "status": "online", "ping": "OK"},
     ]
-    
-    # داده‌ها را به صورت JSON به فرانت‌اند ارسال می‌کنیم
     return JsonResponse(data, safe=False)
