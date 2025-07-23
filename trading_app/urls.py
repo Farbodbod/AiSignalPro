6ec0906ff10902a823946ca6dbcdd12726798f56
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-07-23 20:18:38 +0330

    Update urls.py

--- trading_app/urls.py
+++ trading_app/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 # ویو جدید را وارد می‌کنیم
-from core.views import home_page_view, all_data_view, system_status_view
+from core.views import home_page_view, all_data_view, system_status_view, market_overview_view
 
 urlpatterns = [
     path('', home_page_view, name='home'),
     path('admin/', admin.site.urls),
     
-    # آدرس API قدیمی برای تست
     path('api/status/', system_status_view, name='system-status'),
-    
-    # آدرس API جدید و قدرتمند ما
     path('api/data/all/', all_data_view, name='all-data'),
+    
+    # آدرس API جدید برای نمای کلی بازار
+    path('api/market-overview/', market_overview_view, name='market-overview'),
 ]
