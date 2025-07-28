4270de043da536494a9a85796f69f61caf9353ea
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-07-29 00:42:25 +0330

    Update urls.py

--- backend/trading_app/urls.py
+++ backend/trading_app/urls.py
@@ -1,10 +1,12 @@
+# trading_app/urls.py
+
 from django.urls import path
 from core import views
 
 urlpatterns = [
+    # این آدرس‌ها اکنون به توابع async متصل می‌شوند که جنگو به خوبی از آن پشتیبانی می‌کند
     path('api/status/', views.system_status_view, name='system-status'),
     path('api/market-overview/', views.market_overview_view, name='market-overview'),
-    path('api/data/all/', views.all_data_view, name='all-data'),
-    path('api/trades/open/', views.list_open_trades_view, name='list-open-trades'),
     path('api/get-composite-signal/', views.get_composite_signal_view, name='composite-signal'),
+    path('api/trades/open/', views.list_open_trades_view, name='list-open-trades'),
 ]
