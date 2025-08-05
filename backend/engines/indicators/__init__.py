60ece9346e8fe047de4dc67b66d14161555b2374
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-08-05 21:06:41 +0330

    Update __init__.py

--- backend/engines/indicators/__init__.py
+++ backend/engines/indicators/__init__.py
@@ -3,7 +3,5 @@
 from .base import BaseIndicator
 from .rsi import RsiIndicator
 from .macd import MacdIndicator
-from .bollinger import BollingerIndicator # <--- این خط را اضافه کنید
-
-# هر اندیکاتور جدیدی که می‌سازید، در اینجا import کنید
-# from .ichimoku import IchimokuIndicator
+from .bollinger import BollingerIndicator
+from .ichimoku import IchimokuIndicator # <--- این خط را اضافه کنید
