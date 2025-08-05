61448f048c5ff42d0fe5b4ccfe9a21de81ef4f4e
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-08-06 02:30:11 +0330

    Update __init__.py

--- backend/engines/strategies/__init__.py
+++ backend/engines/strategies/__init__.py
@@ -1,5 +1,8 @@
 # engines/strategies/__init__.py
-
 from .base_strategy import BaseStrategy
 from .trend_rider import TrendRiderStrategy
-from .mean_reversion import MeanReversionStrategy # <--- این خط را اضافه کنید
+from .mean_reversion import MeanReversionStrategy
+from .divergence_sniper import DivergenceSniperStrategy
+from .pivot_reversal import PivotReversalStrategy
+from .volume_catalyst import VolumeCatalystStrategy
+from .ichimoku_pro import IchimokuProStrategy # <--- این خط را اضافه کنید
