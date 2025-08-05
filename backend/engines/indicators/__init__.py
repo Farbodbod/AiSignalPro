60b31de1c7715a4dc73e0eec909b00626fcaea53
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-08-05 20:38:22 +0330

    Create __init__.py

---
+++ backend/engines/indicators/__init__.py
@@ -0,0 +1,8 @@
+# engines/indicators/__init__.py
+
+from .base import BaseIndicator
+from .rsi import RsiIndicator
+
+# هر اندیکاتور جدیدی که می‌سازید، در اینجا import کنید
+# from .macd import MacdIndicator
+# from .bollinger import BollingerIndicator
