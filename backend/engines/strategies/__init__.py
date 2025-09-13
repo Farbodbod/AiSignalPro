5f3be8d0fe8547d5b4792de83e082d141b7da75b
Farbodbod <137355370+Farbodbod@users.noreply.github.com>
2025-09-13 21:37:35 +0330

    Update __init__.py

--- backend/engines/strategies/__init__.py
+++ backend/engines/strategies/__init__.py
@@ -22,6 +22,7 @@ from .volume_catalyst import VolumeCatalystPro
 from .volume_reversal import WhaleReversal
 from .vwap_reversion import VwapMeanReversion
 from .oracle_x_pro import OracleXPro
+from .quantum_channel_surfer.py import QuantumChannelSurfer
 # This is the definitive list of our world-class strategies.
 __all__ = [
     'BaseStrategy',
@@ -41,4 +42,5 @@ __all__ = [
     'WhaleReversal',
     'VwapMeanReversion',
     'OracleXPro',
+    'QuantumChannelSurfer',
 ]
